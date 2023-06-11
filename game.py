77524999fbbb8f304d4bb1a96c0bad7c7a45bc1e
William Lee <williamlee0416@gmail.com>
2023-06-11 16:58:26 -0700

    Implement pipe class, including collision logic

--- game.py
+++ game.py
@@ -71,8 +71,50 @@ class Bird:
         window.blit(rotated_image, new_rectangle.topleft)
 
     def get_mask(self):
-        # collisions for objects
+        # for better collisions
         return pygame.mask.from_surface(self.img)
+
+
+class Pipe:
+    GAP = 200
+    VEL = 5 # bird doesn't move forward, pipes move backward to create that illusion
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.top = 0
+        self.bottom = 0
+        self.PIP_TOP = pygame.transform.flip(PIPE_IMG, False, True) # flip the pipe image for the upper one
+        self.PIPE_BOTTOM = PIPE_IMG
+
+        self.passed = False # if the bird has already passed through
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIP_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, window):
+        window.blit(self.PIP_TOP, (self.x, self.top))
+        window.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset) # returns None if no colllision
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if b_point or t_point:
+            return True
+        return False
 
 
 def draw_window(window, bird):
